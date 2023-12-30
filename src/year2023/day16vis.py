cb6c3c4ed4eef6efebbd323154e13118fd3f320b
Daria Vasyukova <daria.voznyak@gmail.com>
2023-12-31 01:33:49 +0300

    Day 16 vis more or less ready

--- src/year2023/day16vis.py
+++ src/year2023/day16vis.py
@@ -3,10 +3,10 @@
 import sys
 import time
 from random import randint
-from math import floor, ceil, sin, sqrt
+from math import floor, ceil, sin, sqrt, pi
 from aocd import get_data
 import pygame as pg
-from year2023.day16 import example_input, direction_transforms
+from year2023.day16 import example_input, direction_transforms, MirrorsCave
 from util.inputs import movechars_dr_dc
 
 SILVER = (153, 153, 204)
@@ -40,6 +40,7 @@ def color_noise(n, amount=0.1):
 
 class MirrorsCavern:
     def __init__(self, text, maxwidth, maxheight):
+        self.calc_cave = MirrorsCave(text)
         self.lines = text.split("\n")
         # size up and prepare background
         self.maxwidth = maxwidth
@@ -49,6 +50,7 @@ class MirrorsCavern:
         self.unit = int(min(maxwidth / (self.C + 2), maxheight / (self.R + 2)))
         self.speed_of_light = 10
         self.surface = pg.Surface(((self.C + 2) * self.unit, (self.R + 2) * self.unit))
+        # background surface has borders square
         self.background = self.surface.copy()
         self.background.fill(BACKGROUND)
         pg.draw.rect(
@@ -58,8 +60,10 @@ class MirrorsCavern:
             self.unit,
         )
         self.line_width = max(2, self.unit // 4)
+        # mirrors surface holds mirrors and splitters
+        mirrors_line_width = self.line_width + 2
         self.mirrors = self.surface.convert_alpha()
-        self.mirrors.fill((0, 0, 0, 0))
+        self.mirrors.fill(TRANSPARENT)
         for r, line in enumerate(self.lines):
             for c, char in enumerate(line):
                 if char == ".":
@@ -68,11 +72,19 @@ class MirrorsCavern:
                 y0, y1 = self.unit * (r + 1), self.unit * (r + 2)
                 if char == "/":
                     pg.draw.line(
-                        self.mirrors, TEXT_COLOR, (x0, y1), (x1, y0), self.line_width
+                        self.mirrors,
+                        TEXT_COLOR,
+                        (x0, y1),
+                        (x1, y0),
+                        mirrors_line_width,
                     )
                 elif char == "\\":
                     pg.draw.line(
-                        self.mirrors, TEXT_COLOR, (x0, y0), (x1, y1), self.line_width
+                        self.mirrors,
+                        TEXT_COLOR,
+                        (x0, y0),
+                        (x1, y1),
+                        mirrors_line_width,
                     )
                 elif char == "-":
                     pg.draw.line(
@@ -80,7 +92,7 @@ class MirrorsCavern:
                         TEXT_COLOR,
                         (x0, (y0 + y1) / 2),
                         (x1, (y0 + y1) / 2),
-                        self.line_width,
+                        mirrors_line_width,
                     )
                 elif char == "|":
                     pg.draw.line(
@@ -88,22 +100,21 @@ class MirrorsCavern:
                         TEXT_COLOR,
                         ((x0 + x1) / 2, y0),
                         ((x0 + x1) / 2, y1),
-                        self.line_width,
+                        mirrors_line_width,
                     )
-        self.lava = self.surface.copy()
-        self.temp = self.surface.copy()
-        self.beam_surface = self.surface.convert_alpha()
+        # lava surface is self explaining...
+        self.lava = self.surface.convert_alpha()
         self.lava.fill(TRANSPARENT)
+
+        self.temp = self.surface.copy()
+        # beam surface - used for rendering light beams
+        self.beam_surface = self.surface.convert_alpha()
+        self.beam_surface.fill(TRANSPARENT)
         self.energized = set()
         self.lava_colors = {}
         self.rect = self.surface.get_rect()
-        self.origins = (
-            [tuple((r, -1, ">")) for r in range(0, self.R)]
-            + [tuple((self.R, c, "^")) for c in range(0, self.C)]
-            + [tuple((r, self.C, "<")) for r in range(self.R - 1, -1, -1)]
-            + [tuple((-1, c, "v")) for c in range(self.C - 1, -1, -1)]
-        )
-        self.origin_results = [-1] * len(self.origins)
+        self.origins = list(self.calc_cave.iter_origins())
+        self.origin_results = []
         self.best_origin_index = -1
         self.best_energized = 0
         self.total_distance = 0
@@ -165,10 +176,11 @@ class MirrorsCavern:
             if not turned:
                 traveling.add((r0, c0, direction, distance0))
         self.traveling = traveling
+        # print(self.beams, self.traveling)
 
         # update display
-        self.surface.blit(self.background, self.background.get_rect())
-        if len(self.traveling) > 0:
+        self.surface.blit(self.background, self.rect)
+        if self.lightup_time == -1:
             self.beam_surface.fill(TRANSPARENT)
             for beam in self.beams:
                 (r0, c0), (r1, c1), distance_before = beam
@@ -208,10 +220,9 @@ class MirrorsCavern:
                 pg.Rect(0, 0, (self.C + 2) * self.unit, (self.R + 2) * self.unit),
                 self.unit,
             )
-            self.surface.blit(self.beam_surface, self.rect, None, 0)
-        else:
-            if self.lightup_time == -1:
-                print(len(self.energized))
+            self.surface.blit(self.beam_surface, self.rect)
+
+            if len(self.traveling) == 0:
                 self.origin_results[self.origin_index] = len(self.energized)
                 if len(self.energized) > self.best_energized:
                     self.best_energized = len(self.energized)
@@ -226,6 +237,7 @@ class MirrorsCavern:
                         color_noise(blue),
                         alpha,
                     )
+        else:
             self.lightup_time += dt
             if self.do_lava:
                 self.lava.fill(TRANSPARENT)
@@ -269,9 +281,9 @@ class MirrorsCavern:
                 self.temp.set_alpha(255 * min(1, self.lightup_time / 3))
                 self.surface.blit(self.temp, self.rect)
             else:
-                self.surface.blit(self.beam_surface, self.rect, None, pg.BLEND_ADD)
+                self.surface.blit(self.beam_surface, self.rect)
 
-        self.surface.blit(self.mirrors, self.mirrors.get_rect(), None, 0)
+        self.surface.blit(self.mirrors, self.rect)
 
     def light_up(self, origin_index, do_lava=True):
         self.origin_index = origin_index
@@ -284,13 +296,40 @@ class MirrorsCavern:
         self.do_lava = do_lava
         self.energized = set()
 
-    def get_light_source_position(self, light_source):
-        r, c, direction = self.origins[self.origin_index]
-        rotated = light_source
-        rect = light_source.get_rect(
-            right=self.unit * (c + 2), centery=self.unit * (r + 1.5)
-        )
+    def get_light_source_position(self, light_source, origin_index=None):
+        if origin_index is None:
+            origin_index = self.origin_index
+        r, c, direction = self.origins[origin_index]
+        if direction == ">":
+            rotated = light_source
+            rect = rotated.get_rect(
+                right=self.unit * (c + 2), centery=self.unit * (r + 1.5)
+            )
+        elif direction == "<":
+            rotated = pg.transform.rotate(light_source, 180)
+            rect = rotated.get_rect(
+                left=self.unit * (c + 1), centery=self.unit * (r + 1.5)
+            )
+        elif direction == "^":
+            rotated = pg.transform.rotate(light_source, 90)
+            rect = rotated.get_rect(
+                centerx=self.unit * (c + 1.5), top=self.unit * (r + 1)
+            )
+        elif direction == "v":
+            rotated = pg.transform.rotate(light_source, -90)
+            rect = rotated.get_rect(
+                centerx=self.unit * (c + 1.5), bottom=self.unit * (r + 2)
+            )
         return rotated, rect
+
+    def calc_another_origin(self):
+        if len(self.origin_results) < len(self.origins):
+            origin = self.origins[len(self.origin_results)]
+            cnt = self.calc_cave.count_energized_tiles(origin)
+            self.origin_results.append(cnt)
+            if cnt > self.best_energized:
+                self.best_energized = cnt
+                self.best_origin_index = len(self.origin_results) - 1
 
 
 def run():
@@ -299,7 +338,7 @@ def run():
     pg.display.set_caption("Advent of Code 2023 - Day 16 - The Floor Will Be Lava")
 
     text_input = example_input
-    # text_input = get_data(year=2023, day=16)
+    text_input = get_data(year=2023, day=16)
     cave = MirrorsCavern(text_input, WIDTH, HEIGHT)
 
     WP = (WIDTH - cave.surface.get_rect().width) / 2
@@ -309,17 +348,23 @@ def run():
 
     clock = pg.time.Clock()
     # prepare light source
-    light_source = pg.Surface((LIGHT_SOURCE_WIDTH, LIGHT_SOURCE_HEIGHT))
-    light_source.fill(TRANSPARENT)
-    pg.draw.polygon(
-        light_source,
-        WHITE,
-        [
-            (0, 0),
-            (LIGHT_SOURCE_WIDTH, LIGHT_SOURCE_HEIGHT / 2),
-            (0, LIGHT_SOURCE_HEIGHT),
-        ],
-    )
+    light_sources = {}
+    for color in (WHITE, SILVER, GOLD):
+        light_source = pg.Surface(
+            (LIGHT_SOURCE_WIDTH, LIGHT_SOURCE_HEIGHT)
+        ).convert_alpha()
+        light_source.fill(TRANSPARENT)
+        pg.draw.polygon(
+            light_source,
+            color,
+            [
+                (0, 0),
+                (LIGHT_SOURCE_WIDTH, LIGHT_SOURCE_HEIGHT / 2),
+                (0, LIGHT_SOURCE_HEIGHT),
+                (LIGHT_SOURCE_WIDTH // 4, LIGHT_SOURCE_HEIGHT / 2),
+            ],
+        )
+        light_sources[color] = light_source
 
     t0 = time.perf_counter()
     t = 0
@@ -327,12 +372,18 @@ def run():
     light_finished_at = -1
     origin_index = -1
 
+    part1_total = 0
+    part2_total = 0
+    current_total = 0
+
     running = True
 
-    cave.speed_of_light = 0.5 * (cave.R + cave.C)
+    cave.speed_of_light = 0.2 * (cave.R + cave.C)
     # main loop
     while running:
         clock.tick(FPS)
+
+        cave.calc_another_origin()
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -345,24 +396,100 @@ def run():
         last_t = t
 
         if len(cave.traveling) == 0 and light_finished_at == -1:
+            if part1_total == 0:
+                part1_total = len(cave.energized)
+            if part2_total < len(cave.energized):
+                part2_total = len(cave.energized)
             light_finished_at = t
-            origin_index = (origin_index + 1) % len(cave.origins)
-            # origin_index = 0
+            if part1_total == 0:
+                origin_index = 0
+            elif cave.do_lava:
+                light_finished_at = -1
+            else:
+                origin_index = next(
+                    (
+                        i
+                        for i, cnt in enumerate(cave.origin_results)
+                        if cnt > part2_total
+                    ),
+                    origin_index,
+                )
+                # origin_index = (origin_index + 120) % len(cave.origins)
         if light_finished_at != -1 and t - light_finished_at > 2:
-            cave.light_up(origin_index, do_lava=False)
+            cave.light_up(origin_index, do_lava=origin_index == cave.best_origin_index)
             light_finished_at = -1
+            current_total = 0
 
         # erase everything
         screen.fill(BACKGROUND)
 
         # draw platform
         cave.update(dt)
+        current_total = len(cave.energized)
 
         screen.blit(cave.surface, cave_rect)
-        rotated_light_source, rect = cave.get_light_source_position(light_source)
-        rect.top += HP
-        rect.left += WP
-        screen.blit(rotated_light_source, rect, None, pg.BLEND_MAX)
+
+        if part1_total > 0:
+            # render part 1 light source and total
+            rotated_light_source, rect = cave.get_light_source_position(
+                light_sources[SILVER], origin_index=0
+            )
+            rect.top += HP
+            rect.left += WP
+            screen.blit(rotated_light_source, rect)
+            total1 = font.render(
+                f"Part 1: {part1_total}",
+                1,
+                SILVER,
+            )
+            if rect.centerx < WIDTH // 2:
+                total1_rect = total1.get_rect(
+                    centery=rect.centery, right=rect.left - 10
+                )
+            else:
+                total1_rect = total1.get_rect(
+                    centery=rect.centery, left=rect.right + 10
+                )
+            screen.blit(total1, total1_rect)
+
+        # render part2 light source
+        if cave.do_lava and cave.lightup_time >= 0:
+            rotated_light_source, rect = cave.get_light_source_position(
+                light_sources[GOLD], cave.best_origin_index
+            )
+            rect.top += HP
+            rect.left += WP
+            screen.blit(rotated_light_source, rect)
+            total2 = font.render(
+                f"Part 2: {part2_total}",
+                1,
+                GOLD,
+            )
+            if rect.centerx < WIDTH // 2:
+                total2_rect = total2.get_rect(
+                    centery=rect.centery, right=rect.left - 10
+                )
+            else:
+                total2_rect = total2.get_rect(
+                    centery=rect.centery, left=rect.right + 10
+                )
+            screen.blit(total2, total2_rect)
+        else:
+            # render current light source
+            rotated_light_source, rect = cave.get_light_source_position(
+                light_sources[WHITE]
+            )
+            rect.top += HP
+            rect.left += WP
+            screen.blit(rotated_light_source, rect)
+
+            total = font.render(str(current_total), 1, WHITE)
+            if rect.left <= WIDTH // 2:
+                total_rect = total.get_rect(right=rect.left - 10, centery=rect.centery)
+            else:
+                total_rect = total.get_rect(left=rect.right + 10, centery=rect.centery)
+            screen.blit(total, total_rect)
+
         # actually update the screen now
         pg.display.flip()
 
