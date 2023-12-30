75a2837d0c62da44e7e99279c04f03fb04a624c2
Daria Vasyukova <daria.voznyak@gmail.com>
2023-12-31 01:33:28 +0300

    Refactor day 16 - Make the solution faster using caching - Make it more vis-friendly too

--- src/year2023/day16.py
+++ src/year2023/day16.py
@@ -1,5 +1,6 @@
 # Problem statement: https://adventofcode.com/2023/day/16
 
+from functools import cache
 from util.inputs import movechars_dr_dc
 
 day_title = "The Floor Will Be Lava"
@@ -27,60 +28,67 @@ direction_transforms = {
 }
 
 
+class MirrorsCave:
+    def __init__(self, text):
+        self.lines = text.split("\n")
+        self.R = len(self.lines)
+        self.C = len(self.lines[0])
+
+    @cache
+    def process_beam(self, origin):
+        r, c, direction = origin
+        energized = set()
+        dr, dc = movechars_dr_dc[direction]
+        newdirs = ""
+        while True:
+            energized.add((r, c))
+            r += dr
+            c += dc
+            if r < 0 or r >= self.R or c < 0 or c >= self.C:
+                break
+            newchar = self.lines[r][c]
+            if newchar != ".":
+                newdirs = direction_transforms[newchar][direction]
+                break
+        new_origins = [(r, c, newdir) for newdir in newdirs]
+        return energized, new_origins
+
+    def count_energized_tiles(self, origin):
+        processed = set()
+        origins = set((origin,))
+        energized = set()
+        while len(origins) > 0:
+            origin = origins.pop()
+            processed.add(origin)
+            new_energized, new_origins = self.process_beam(origin)
+            energized.update(new_energized)
+            origins.update(o for o in new_origins if o not in processed)
+        return len(energized) - 1
+
+    def iter_origins(self):
+        for r in range(0, self.R):
+            yield (r, -1, ">")
+        for c in range(0, self.C):
+            yield (self.R, c, "^")
+        for r in range(self.R, -1, -1):
+            yield (r, self.C, "<")
+        for c in range(self.C, -1, -1):
+            yield (-1, c, "v")
+
+
 def part1(text_input):
-    origin = tuple((0, -1, ">"))
-    lines = text_input.split("\n")
-    energized = count_energized_tiles(lines, origin)
+    cave = MirrorsCave(text_input)
+    origin = next(cave.iter_origins())
+    energized = cave.count_energized_tiles(origin)
     return energized
 
 
-def count_energized_tiles(lines, origin):
-    processed = set()
-    origins = set((origin,))
-    energized = set()
-    while len(origins) > 0:
-        origin = origins.pop()
-        processed.add(origin)
-        r, c, direction = origin
-        dr, dc = movechars_dr_dc[direction]
-        newdirs = direction
-        while newdirs == direction:
-            energized.add((r, c))
-            r = r + dr
-            c = c + dc
-            if r < 0 or r >= len(lines) or c < 0 or c >= len(lines[0]):
-                newdirs = ""
-                break
-            newchar = lines[r][c]
-            if newchar == ".":
-                continue
-            newdirs = direction_transforms[newchar][direction]
-            if newdirs == direction:
-                # splitter sideways
-                processed.add((r, c, direction))
-        for newdir in newdirs:
-            neworigin = tuple((r, c, newdir))
-            if neworigin not in processed:
-                origins.add(neworigin)
-    return len(energized) - 1
-
-
 def part2(text_input):
+    cave = MirrorsCave(text_input)
     best = 0
-    lines = text_input.split("\n")
-    R = len(lines)
-    C = len(lines[0])
-    origins = (
-        [tuple((-1, c, "v")) for c in range(0, C)]
-        + [tuple((R, c, "^")) for c in range(0, C)]
-        + [tuple((r, -1, ">")) for r in range(0, R)]
-        + [tuple((r, C, "<")) for r in range(0, R)]
-    )
-    for origin in origins:
-        cnt = count_energized_tiles(lines, origin)
-        if cnt > best:
-            # print(origin, cnt)
-            best = cnt
+    for origin in cave.iter_origins():
+        cnt = cave.count_energized_tiles(origin)
+        best = max(best, cnt)
     return best
 
 
