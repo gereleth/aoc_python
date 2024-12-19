54fcccee9eeee3018a58f3740c48438250bd1d2e
Daria Vasyukova <daria.voznyak@gmail.com>
2024-12-19 13:27:41 +0300

    A tree index for towels (lol)
    Day 19 now runs in 92 ms for me

--- src/year2024/day19.py
+++ src/year2024/day19.py
@@ -1,24 +1,50 @@
 # Problem statement: https://adventofcode.com/2024/day/19
 
 from collections import Counter, defaultdict
+from typing import Iterator
 
 day_title = "Linen Layout"
 
 
+# A data structure for quick iteration over towels that a particular
+# design could start with
+class PentacolorTree:
+    def __init__(self):
+        self.is_leaf = False
+        self.children = defaultdict(PentacolorTree)
+
+    def put(self, value: str) -> None:
+        """Add a towel to the tree"""
+        if len(value) == 0:
+            self.is_leaf = True
+        else:
+            child = self.children[value[0]]
+            child.put(value[1:])
+
+    def get_splits(self, value: str) -> Iterator[tuple[str, str]]:
+        """Splits string into `towel+rest` for all towels that fit"""
+        tree = self
+        for i, char in enumerate(value):
+            if char not in tree.children:
+                break
+            tree = tree.children[char]
+            if tree.is_leaf:
+                yield value[: i + 1], value[i + 1 :]
+
+
 def parse_input(text_input: str):
-    # index towels by their first letter to limit iterations we need to do later
-    # some kind of tree could be even better maybe?
     towels, designs = text_input.split("\n\n")
     towels = towels.split(", ")
-    towels_index = defaultdict(list)
+    # index towels into a tree structure
+    towels_tree = PentacolorTree()
     for towel in towels:
-        towels_index[towel[0]].append(towel)
+        towels_tree.put(towel)
     designs = designs.split()
-    return towels_index, designs
+    return towels_tree, designs
 
 
 def part1(text_input: str) -> int:
-    towels_index, designs = parse_input(text_input)
+    towels_tree, designs = parse_input(text_input)
     total = 0
     # canmake is a dict like {design: can we make it}
     canmake = defaultdict(bool)
@@ -30,8 +56,8 @@ def part1(text_input: str) -> int:
             if tail in canmake:
                 continue
             # see if `tail` is `some_towel + rest` where we can make `rest`
-            for towel in towels_index[tail[0]]:
-                if tail.startswith(towel) and canmake[tail[len(towel) :]]:
+            for _, rest in towels_tree.get_splits(tail):
+                if canmake[rest]:
                     canmake[tail] = True
                     break
         total += canmake[design]
@@ -39,7 +65,7 @@ def part1(text_input: str) -> int:
 
 
 def part2(text_input: str) -> int:
-    towels_index, designs = parse_input(text_input)
+    towels_tree, designs = parse_input(text_input)
     total = 0
 
     ways = Counter()
@@ -50,9 +76,8 @@ def part2(text_input: str) -> int:
             tail = design[-i:]
             if tail in ways:
                 continue
-            for towel in towels_index[tail[0]]:
-                if tail.startswith(towel):
-                    ways[tail] += ways[tail[len(towel) :]]
+            for _, rest in towels_tree.get_splits(tail):
+                ways[tail] += ways[rest]
         total += ways[design]
     return total
 
